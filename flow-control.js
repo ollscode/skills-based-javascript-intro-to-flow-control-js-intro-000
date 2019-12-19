c9dcef4acdc2da9f9cb9d2de064ccf842b435353
Oscar <mundonatural@gmail.com>
2019-12-19 10:45:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,3 +29,18 @@ if (5 < 3) {
 } else {
   console.log("nada se cumple");
 }
+
+5 < 3 ? console.log("5 es menor que 3") : console.log("5 no es menor que 3");
+
+var valor = 5
+switch (valor) {
+  case 1:
+      console.log("es 1");
+      break;
+  case 5:
+      console.log("es 5");
+      break;
+  default:
+      console.log("nada se cumple");
+}
+
