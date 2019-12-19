eaa411310a2777e475645117c1d00813e16c041d
Oscar <mundonatural@gmail.com>
2019-12-19 10:42:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,3 +17,15 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 
 }
+
+if (5 > 3) {
+  console.log("5 es mayor que 3");
+}
+
+if (5 < 3) {
+  console.log("5 es menor que 3");
+} else if (4 == 2) {
+  console.log("4 es igual que 2");
+} else {
+  console.log("nada se cumple");
+}
